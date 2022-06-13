56b520a2252abd75cb754b9905a14c95c76c388e
artismyname <4lex4andre@gmail.com>
2022-06-13 12:33:16 -0500

    Foi criado o servidor para criação e armazenamento do feedback utilizando prisma, sqlite, testes unitários e um dos principios do SOLID que é o Dependency Inversion Principle

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "fed74c5e74dc37",
+    pass: "d3ebbf2934211d"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Alexandre Merched <4lex4andre@gmail.com>',
+      subject,
+      html: body
+    });
+  }
+}
